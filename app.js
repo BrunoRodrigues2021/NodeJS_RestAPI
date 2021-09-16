63137cb9f52e3bd02e562871376a3e312223e1be
Bruno <brunorodri2015@gmail.com>
2021-09-15 21:35:01 -0300

    tratando erros de rotas

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
 const routeProducts = require('./routes/produtos');
 const routeRequests = require('./routes/pedidos');
 
+app.use(morgan('dev')); //RETORNA UM LOG DAS REQUESTS FEITAS NO SERVIDOR
+
 app.use('/produtos', routeProducts);
 app.use('/pedidos', routeRequests);
+
+//QUANDO NÃO ENCONTRAR ROTA
+app.use((req, res, next) => {
+    const error = new Error('Não Encontrado');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    return res.send({
+        erro: {
+            mensagem: error.message
+        }
+    });
+});
 
 module.exports = app;
